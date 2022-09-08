7c200eae1eb013f3208ce1d99df9a54c79f91772
menna <menaabdo076@gmail.com>
2022-09-08 04:16:16 +0200

    sales update

--- src/app/modules/store/products/store-product-layout.component.ts
+++ src/app/modules/store/products/store-product-layout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from 'src/app/models/product.model';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-store-product-layout',
@@ -8,12 +10,19 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class StoreProductLayoutComponent implements OnInit {
 id!:number
-  constructor(private activeroute:ActivatedRoute,private route :Router) {
+term=''
+response!:any
+ads!:any
+  constructor(private productservice:UserService,private activeroute:ActivatedRoute,private route :Router) {
     this.id=this.activeroute.snapshot.params['id']
    }
 
   ngOnInit(): void {
   this.route.navigateByUrl(`/home/me/store/${this.id}/myproducts/${this.id}/active-ads/${this.id}`)
+this.getproducts() 
+}
+  getproducts(){
+    this.productservice.products(this.id,this.term).subscribe((res)=>{this.response=res;this.ads=this.response.Response})
+    
   }
-
 }
